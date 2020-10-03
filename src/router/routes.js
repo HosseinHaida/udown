71007bd78419cdf789a0a45d25b861e6d472c92e
Hossein Heidari <squanderness@gmail.com>
2020-10-03 09:33:51 +0330

    new event and event page router setup

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,11 @@ const routes = [
       { path: "/courts", component: () => import("pages/CourtsList.vue") },
       { path: "/courts/:id", component: () => import("pages/Court.vue") },
       { path: "/events", component: () => import("pages/EventsList.vue") },
+      {
+        path: "/events/new/:location_id",
+        component: () => import("pages/NewEvent.vue")
+      },
+      { path: "/events/:id", component: () => import("pages/Event.vue") },
       { path: "/players", component: () => import("pages/PlayersList.vue") },
       { path: "/profile", component: () => import("pages/UserProfile.vue") },
       { path: "", redirect: "/courts" }
