e4d58d0f6989349b964fa926e1cb953b909fdc27
Hossein Heidari <squanderness@gmail.com>
2020-10-03 09:23:43 +0330

    login layout header and comment remove

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -3,17 +3,13 @@
     <q-header elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title>
-          <q-avatar>
+          <q-avatar to="/">
             <img src="udown-logo.png" />
           </q-avatar>
-          <q-btn dense to="/" size="23px" flat label="U Down?" no-caps />
+          <!-- <q-btn dense to="/" size="23px" flat label="U Down?" no-caps /> -->
+          U Down?
         </q-toolbar-title>
       </q-toolbar>
-      <!-- <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One" />
-        <q-route-tab to="/page2" label="Page Two" />
-        <q-route-tab to="/page3" label="Page Three" />
-      </q-tabs> -->
     </q-header>
     <q-page-container>
       <router-view />
