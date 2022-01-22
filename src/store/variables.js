678a2d455999544d3a2d45fe49c278d60bea6cba
Hossein Heidari <squanderness@gmail.com>
2022-01-22 21:05:31 +0330

    api link to local

--- src/store/variables.js
+++ src/store/variables.js
@@ -1,2 +1,2 @@
-export const apiUrl = "https://udown.site/api/v1";
-// export const apiUrl = "http://localhost:3060/api/v1";
+// export const apiUrl = "https://udown.site/api/v1";
+export const apiUrl = "http://localhost:3060/api/v1";
